e4ee009b00ee6c65b407fa0fb781c8e823abf4bd
mifan <mifanxp@gmail.com>
2009-08-12 18:38:17 +0800

    refactoring common javascripts about right menu and app selector

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,24 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {  
+
+            //right column click handler
+			$(".collapsible .sidebarTitle").click(function(){
+				$(this).parent().toggleClass("collapsed").find(".sidebarMenu").slideToggle("fast");
+				return false;
+			});
+
+
+			//app selector event handlers
+			// hover
+			$(".UISelectBox").hover(function(){
+					$(this).addClass("UISelectBoxHover");
+				},function(){
+					$(this).removeClass("UISelectBoxHover");
+			});
+			//click
+			$(".UISelectBox").click(function(){
+				$(this).blur();	
+				//$(".UISelectBox .UISelectBoxOptions").toggle();
+				$(".UISelectBox .UISelectBoxOptions").slideToggle("fast");
+				return false;
+			});
+});
