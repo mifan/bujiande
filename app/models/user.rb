cb9cf747ebff1c7ea69ff13ee2ea07a50009f62c
mifan <mifanxp@gmail.com>
2009-09-06 22:41:28 +0800

    add more configuration to authlogic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   acts_as_authentic do |c|
     c.require_password_confirmation = false
+    c.validates_length_of_login_field_options = {:within => 2..20}
   end
 
 end
